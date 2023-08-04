397fc3395c7252990a3fb97fdf1b550dc7741b45
Shentori <103909502+Shentori@users.noreply.github.com>
2023-08-04 09:31:03 -0500

    logout

---
+++ pages/logout/logout.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function LoginPage() {
+
+
+return (
+
+    <div className="bg-blue-300 h-screen flex items-center justify-center">
+
+
+<button className="btn bg-blue-400 w-98 ">BOTON EQUIS</button>
+
+
+
+
+</div>
+
+
+)
+
+
+
+}
